71b156e5cd54012b658def5db05325ca513112ec
tbrunetti <brunettt@colorado.edu>
2022-09-23 17:04:13 -0600

    updated argument parser and tested on old data set

--- code/plot_mutational_frequency_and_qc_stats.py
+++ code/plot_mutational_frequency_and_qc_stats.py
@@ -2,6 +2,7 @@ import pandas
 import argparse
 import typing
 import numpy
+import os
 from matplotlib.ticker import ScalarFormatter
 
 # since ScalarFormatterClss does not allow flexibility in formatting
@@ -48,7 +49,7 @@ def filter(df: pandas.DataFrame, minQ: float, minAlt: int, codonRange: str) -> p
     return df
 
 
-def get_per_codon_ntNum_mutational_freq(df_hash : dict) -> None:
+def get_per_codon_ntNum_mutational_freq(df_hash : dict, outdir : str) -> None:
     import matplotlib.pyplot as plt
     
     colors = {}
@@ -142,15 +143,10 @@ def get_per_codon_ntNum_mutational_freq(df_hash : dict) -> None:
         #ax4.ylabel('Freqency of mutation', fontweight = 'bold', color = 'darkblue', fontsize = '10', horizontalalignment = 'center')
 
         combined_fig.tight_layout()
-        combined_fig.savefig('freq_of_nt_changes_per_codon_{}.png'.format(key), dpi=600, format = 'png')
+        combined_fig.savefig(os.path.join(outdir, 'freq_of_nt_changes_per_codon_{}.png'.format(key)), dpi=600, format = 'png')
+        
 
-    # only use if x and y axis are scaled the same for all plots
-    '''
-    for ax in combined_fig.get_axes():
-        ax.label_outer()
-    '''
-
-def get_per_codon_aaTypeChange_mutational_freq(df_hash: dict) -> None:
+def get_per_codon_aaTypeChange_mutational_freq(df_hash: dict, outdir : str) -> None:
     import matplotlib.pyplot as plt
     import matplotlib.ticker as mtick
     from matplotlib.lines import Line2D
@@ -239,7 +235,7 @@ def get_per_codon_aaTypeChange_mutational_freq(df_hash: dict) -> None:
         axs[1,1].tick_params(axis='y', labelsize= 9)
         
         combined_fig.tight_layout()
-        combined_fig.savefig('freq_of_aa_changes_per_codon_lineplot_{}.png'.format(key), dpi=600, format = 'png')
+        combined_fig.savefig(os.path.join(outdir, 'freq_of_aa_changes_per_codon_lineplot_{}.png'.format(key)), dpi=600, format = 'png')
 
 
     combined_sample_fig, axs = plt.subplots(1, 3, figsize=(9, 3))
@@ -284,10 +280,10 @@ def get_per_codon_aaTypeChange_mutational_freq(df_hash: dict) -> None:
     axs[2].legend([Line2D([0], [0], color=colors[0], lw=4),
                 Line2D([0], [0], color=colors[1], lw=4)], legend_labels)
     combined_sample_fig.tight_layout()
-    combined_sample_fig.savefig('freq_of_aa_changes_per_codon_lineplot_sample_overlay.png', dpi=600, format = 'png')
+    combined_sample_fig.savefig(os.path.join(outdir, 'freq_of_aa_changes_per_codon_lineplot_sample_overlay.png'), dpi=600, format = 'png')
 
 
-def get_per_sample_aaTypeChange_mutational_freq_stackedBarPlot(df_hash: dict) -> None:
+def get_per_sample_aaTypeChange_mutational_freq_stackedBarPlot(df_hash: dict, outdir : str) -> None:
     
     import matplotlib.pyplot as plt
     samples = []
@@ -356,10 +352,10 @@ def get_per_sample_aaTypeChange_mutational_freq_stackedBarPlot(df_hash: dict) ->
     plt.gca().yaxis.set_major_formatter(yScalarFormatter)
     plt.tick_params(axis='y', labelsize= 9)
     plt.tight_layout()
-    plt.savefig('freq_of_aa_changes_across_codons_all_samples_stackedBarPlot.png', dpi=600, format = 'png')
+    plt.savefig(os.path.join(outdir, 'freq_of_aa_changes_across_codons_all_samples_stackedBarPlot.png'), dpi=600, format = 'png')
 
 
-def get_per_sample_ntNum_mutational_freq_stackedBarPlot(df_hash: dict, nonsynOnly: bool) -> None:
+def get_per_sample_ntNum_mutational_freq_stackedBarPlot(df_hash: dict, nonsynOnly: bool, outdir : str) -> None:
     import matplotlib.pyplot as plt
 
     one = [] # frequencies of zeroes are included at every codon position
@@ -437,10 +433,10 @@ def get_per_sample_ntNum_mutational_freq_stackedBarPlot(df_hash: dict, nonsynOnl
         axs.set_title('Mean frequency of nucleotide mutation types \n across all codons')
 
     combined_fig.tight_layout()
-    combined_fig.savefig('freq_of_nt_changes_across_codons_all_samples_stackedBarPlot.png', dpi=600, format = 'png')
+    combined_fig.savefig(os.path.join(outdir, 'freq_of_nt_changes_across_codons_all_samples_stackedBarPlot.png'), dpi=600, format = 'png')
 
     
-def get_per_codon_aa_mutational_information_logoplot(df_hash: dict, nonsynOnly: bool) -> None:
+def get_per_codon_aa_mutational_information_logoplot(df_hash: dict, nonsynOnly: bool, outdir: str) -> None:
     import logomaker
     import matplotlib.pyplot as plt
     import math
@@ -509,10 +505,10 @@ def get_per_codon_aa_mutational_information_logoplot(df_hash: dict, nonsynOnly: 
             end=end+increment
 
         fig.tight_layout()
-        fig.savefig('logoplot_of_mutations_information_{}.png'.format(key), dpi=600, format = 'png')
+        fig.savefig(os.path.join(outdir, 'logoplot_of_mutations_information_{}.png'.format(key)), dpi=600, format = 'png')
 
 
-def get_coverage_per_codon(df_hash : dict) -> None:
+def get_coverage_per_codon(df_hash : dict, outdir : str) -> None:
     import matplotlib.pyplot as plt
     for key, value in df_hash.items():
         fig = plt.figure()
@@ -529,14 +525,14 @@ def get_coverage_per_codon(df_hash : dict) -> None:
         plt.xlabel('Codon position', fontweight = 'bold', color = 'darkblue', fontsize = '10', horizontalalignment = 'center')
         plt.ylabel('Depth \n(avg reads per codon)', fontweight = 'bold', color = 'darkblue', fontsize = '10', horizontalalignment = 'center')
         fig.tight_layout()
-        fig.savefig('depth_of_coverage_{}.png'.format(key), dpi=600, format = 'png')
+        fig.savefig(os.path.join(outdir, 'depth_of_coverage_{}.png'.format(key)), dpi=600, format = 'png')
 
 
 def get_coverage_per_base():
     pass
 
 
-def testing_stacked_barplot(df_hash: dict, nonsynOnly: bool, removeStop: bool) -> None:
+def get_combined_mutational_frequencies_stacked_barplot(df_hash: dict, nonsynOnly: bool, includeStop: bool, outdir : str) -> None:
     import matplotlib.pyplot as plt
     import gc
 
@@ -553,7 +549,7 @@ def testing_stacked_barplot(df_hash: dict, nonsynOnly: bool, removeStop: bool) -
             print('Removing synonymous amino acid changes')
             value = value.loc[value['REF_AA'] != value['AA']]
         
-        if removeStop:
+        if includeStop == False:
             print('Removing stop codons')
             value = value.loc[value['AA'] != '.']
 
@@ -673,7 +669,7 @@ def testing_stacked_barplot(df_hash: dict, nonsynOnly: bool, removeStop: bool) -
     axs.yaxis.set_major_formatter(yScalarFormatter)
     axs.tick_params(axis='y', labelsize= 9)
 
-    fig.savefig('combined_mutational_frequencies_across_samples_and_mutations_grouped_stackedBarPlot.png', dpi=600, format = 'png')
+    fig.savefig(os.path.join(outdir, 'combined_mutational_frequencies_across_samples_and_mutations_grouped_stackedBarPlot.png'), dpi=600, format = 'png')
 
     '''
     x_axis = numpy.arange(len(samples))
@@ -697,7 +693,7 @@ def testing_stacked_barplot(df_hash: dict, nonsynOnly: bool, removeStop: bool) -
     '''    
 
 
-def get_aa_diversity(df_hash : dict) -> None:
+def get_aa_diversity(df_hash : dict, outdir : str) -> None:
     import matplotlib.pyplot as plt
     
     colors = {}
@@ -732,10 +728,10 @@ def get_aa_diversity(df_hash : dict) -> None:
         axs.set_xlabel('codon position')
         
     axs.legend(legend_order)
-    combined_fig.savefig('amino_acid_diversity_at_each_codon_across_samples.png', dpi=600, format = 'png')
+    combined_fig.savefig(os.path.join(outdir, 'amino_acid_diversity_at_each_codon_across_samples.png'), dpi=600, format = 'png')
    
 
-def get_per_codon_aa_mutational_freq_logoplot(df_hash: dict, nonsynOnly: bool, includeStop: bool, annot: bool) -> None:
+def get_per_codon_aa_mutational_freq_logoplot(df_hash: dict, nonsynOnly: bool, includeStop: bool, annot: bool, outdir: str) -> None:
     import logomaker
     import matplotlib.pyplot as plt
     import math
@@ -820,7 +816,6 @@ def get_per_codon_aa_mutational_freq_logoplot(df_hash: dict, nonsynOnly: bool, i
                     print("There is no E3!")
                     e3_plot_range_start = None
 
-                print("here!!!!")
                 if e2_plot_range_start != None:
                     ax.plot([e2_plot_range_start, e2_plot_range_end+1], [y, y], color = "lightblue", linewidth = 10, solid_capstyle="butt")
                     ax.text(((e2_plot_range_start + e2_plot_range_end)/2), y*1.4,'E2',fontsize=12)
@@ -851,13 +846,10 @@ def get_per_codon_aa_mutational_freq_logoplot(df_hash: dict, nonsynOnly: bool, i
 
         fig.tight_layout()
         if includeStop:
-            fig.savefig('logoplot_of_mutations_freq_{}_including_stop_codons.png'.format(key), dpi=600, format = 'png')
+            fig.savefig(os.path.join(outdir, 'logoplot_of_mutations_freq_{}_including_stop_codons.png'.format(key)), dpi=600, format = 'png')
         else:
-            fig.savefig('logoplot_of_mutations_freq_{}_excluding_stop_codons.png'.format(key), dpi=600, format = 'png')
+            fig.savefig(os.path.join(outdir, 'logoplot_of_mutations_freq_{}_excluding_stop_codons.png'.format(key)), dpi=600, format = 'png')
             
-
-
-
 
 if __name__ == '__main__':
     
@@ -867,10 +859,13 @@ if __name__ == '__main__':
     
     parser.add_argument('--data', required = True, action = 'extend', nargs = '+', type = str, help = 'list of all codon tables per sample as generated by virVar')
     parser.add_argument('--qual', default = 24.0, type = float, help = 'A float specifying the minimum average quality of reads to keep for filtering of results')
-    parser.add_argument('--counts', default = 100, help = 'minimum average number of counts for a codon variant to keep post filtering')
+    parser.add_argument('--counts', default = 100, type = int, help = 'minimum average number of counts for a codon variant to keep post filtering')
     parser.add_argument('--pos', default = "0-0", type = str, help= 'integer range of codon positions to use in analysis. Default is to use all availble. Ex: 7-100, would include codon 7 through codon 100, inclusive on both ends based upon your input fasta file. WARNING: makes assumption that codon 1 is the first 3 bp of your input fasta reference file')
     parser.add_argument('--samplename', help='name of samples, comma-separated, in the same order as the list of input data')
-
+    parser.add_argument('--nonSynOnly', action = 'store_true', help='If setting this flag, only nonsynonymous mutations will be considered')
+    parser.add_argument('--includeStop', action = 'store_true', help='If setting this flag, include STOP codons')
+    parser.add_argument('--annotate', action = 'store_true', help='DO NOT SET THIS -- experimental and needs testing')
+    parser.add_argument('--outdir', default = os.getcwd(), help = "Path to output directory to write plots")
     args = parser.parse_args()
     
     df_hash = {} # dict to store each samples data as a panda df
@@ -895,21 +890,30 @@ if __name__ == '__main__':
     
     
     # The colorblind friendly palette with grey:
-    cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    #cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
     # The colorblind friendly palette with black:
-    cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    #cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    
+    
+    if os.path.exists(args.outdir):
+        pass
+    else:
+        os.mkdir(args.outdir)
+    
     
     for index,samples in enumerate(args.data):
         sample_df = pandas.read_csv(samples, sep = '\t')
         sample_df = filter(df = sample_df, minQ = args.qual, minAlt = args.counts, codonRange = args.pos)
-        df_hash[args.samplename[index]] = sample_df
+        df_hash[args.samplename.split(',')[index].strip()] = sample_df
 
         
-        
-    get_per_codon_ntNum_mutational_freq(df_hash = df_hash)
-    get_per_codon_aaTypeChange_mutational_freq(df_hash = df_hash)
-    
-        
-        
-                        
+    get_per_codon_ntNum_mutational_freq(df_hash = df_hash, outdir = args.outdir)
+    get_per_codon_aaTypeChange_mutational_freq(df_hash = df_hash, outdir = args.outdir)
+    get_per_sample_aaTypeChange_mutational_freq_stackedBarPlot(df_hash = df_hash, outdir = args.outdir)
+    get_per_sample_ntNum_mutational_freq_stackedBarPlot(df_hash = df_hash, nonsynOnly = args.nonSynOnly, outdir = args.outdir)
+    get_per_codon_aa_mutational_information_logoplot(df_hash = df_hash, nonsynOnly = args.nonSynOnly, outdir = args.outdir)
+    get_coverage_per_codon(df_hash = df_hash, outdir = args.outdir)
+    get_combined_mutational_frequencies_stacked_barplot(df_hash = df_hash, nonsynOnly = args.nonSynOnly, includeStop = args.includeStop, outdir = args.outdir)
+    get_aa_diversity(df_hash = df_hash, outdir = args.outdir)
+    get_per_codon_aa_mutational_freq_logoplot(df_hash = df_hash, nonsynOnly = args.nonSynOnly, includeStop = args.includeStop, annot = args.annotate, outdir = args.outdir)
